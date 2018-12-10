bae84e14dfc6ccd2fc80bc5e137aa291d5c14ee6
ssnyder555 <ssnyder555@live.com>
2018-12-10 11:34:41 -0600

    now added faker

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-let cat  = require('cat-me');
-let joke = require('knock-knock-jokes');
+let cat     = require('cat-me');
+let joke    = require('knock-knock-jokes');
+var faker   = require('faker');
 
-console.log(cat());
 
-console.log(joke());
+var randomName = faker.name.findName(); // Rowan Nikolaus
+var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+var randomCard = faker.helpers.createCard(); // random contact ca
+
+console.log(randomName);
+console.log(randomEmail);
+console.log(randomCard);
