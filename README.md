efe181b6cce3bdb9a8e7cfda711d9c38991c4cea
ssnyder555 <ssnyder555@live.com>
2018-12-10 11:19:52 -0600

    first and only commit--- maybe

---
+++ README.md
@@ -0,0 +1,5 @@
+# working with node packages!
+
+## trying out my joke and cat skills
+
+![alt text](/img/SC.png)
---
+++ server.js
@@ -0,0 +1,6 @@
+let cat  = require('cat-me');
+let joke = require('knock-knock-jokes');
+
+console.log(cat());
+
+console.log(joke());
