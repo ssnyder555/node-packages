094eef7ebf3ee832a3b54137631ae26d3da98d78
ssnyder555 <ssnyder555@live.com>
2018-12-10 11:45:04 -0600

    now with products and prices

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ let cat     = require('cat-me');
 let joke    = require('knock-knock-jokes');
 var faker   = require('faker');
 
-
-var randomName = faker.name.findName(); // Rowan Nikolaus
-var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
-var randomCard = faker.helpers.createCard(); // random contact ca
-
-console.log(randomName);
-console.log(randomEmail);
-console.log(randomCard);
+console.log("===================");
+console.log("Welcome to my shop");
+console.log("====================");
+for (let i = 0; i < 10; i++){
+  console.log(faker.commerce.productName() + ' - $' + faker.commerce.price());
+}
